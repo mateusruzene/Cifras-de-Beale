2ceb03dd1f4a80bb46dd3a197b7b0110632109c6
Mateus Ruzene <mateus.ruzene@mblabs.com.br>
2023-05-08 16:57:36 -0300

    fix

--- libdecodifica.h
+++ libdecodifica.h
@@ -7,7 +7,7 @@
 /* Decodifica a entrada com as chaves e escreve no arquivo de saida*/
 void decodifica(struct chaves_t *chaves, FILE *entrada, FILE *saida);
 
-/* Decodifica para @txtOut o texto em @txtCod com base em @textoBase.*/
+/* Decodifica para a saida o texto de entrada com base no livro de cifras.*/
 int decodifica_com_livro(FILE *livroCifras, FILE *entrada, FILE *saida);
 
 /* Decodifica a entrada com base no arquivo de cifras e escreve no arquivo de saida*/
